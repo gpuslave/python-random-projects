f00c6759e778fcda8fcd532736812e8b55c19e90
SHIMANOEYE <shimanoee@yandex.ru>
2024-09-18 21:31:12 +0300

    refactor class into two separate

--- wc/wc.py
+++ wc/wc.py
@@ -9,66 +9,65 @@ import re
 __version__ = "0.0.1"
 
 
-class WC:
-    def __init__(self, filePath: Union[str, Path, None] = None):
-        if not filePath:
-            self._parser = argparse.ArgumentParser(
-                prog="wc",
-                description="Python implementation of unix utility wc",
-            )
+class WCParser:
+    """CLI for WC class"""
 
-            self._parser.add_argument(
-                "file_name",
-                metavar="path_to_file",
-                type=str,
-            )
+    def __init__(self):
+        self._parser = argparse.ArgumentParser(
+            prog="wc",
+            description="Python implementation of unix utility wc",
+        )
 
-            self._parser.add_argument(
-                "-v",
-                "--version",
-                action="version",
-                version=f"%(prog)s {__version__}",
-            )
+        self._parser.add_argument(
+            "file_name",
+            metavar="path_to_file",
+            type=str,
+        )
 
-            self._parser.add_argument(
-                "-l",
-                "--lines",
-                action="store_true",
-                help="count lines in file",
-            )
+        self._parser.add_argument(
+            "-v",
+            "--version",
+            action="version",
+            version=f"%(prog)s {__version__}",
+        )
 
-            self._parser.add_argument(
-                "-c",
-                "--bytes",
-                action="store_true",
-                help="count bytes in file",
-            )
+        self._parser.add_argument(
+            "-l",
+            "--lines",
+            action="store_true",
+            help="count lines in file",
+        )
 
-            self._parser.add_argument(
-                "-m",
-                "--chars",
-                action="store_true",
-                help="count chars in file",
-            )
+        self._parser.add_argument(
+            "-c",
+            "--bytes",
+            action="store_true",
+            help="count bytes in file",
+        )
 
-            self._parser.add_argument(
-                "-w",
-                "--words",
-                action="store_true",
-                help="count words in file",
-            )
+        self._parser.add_argument(
+            "-m",
+            "--chars",
+            action="store_true",
+            help="count chars in file",
+        )
 
-            self._parser.add_argument(
-                "-L",
-                "--max-line-lenght",
-                action="store_true",
-                help="length of max-length line",
-            )
+        self._parser.add_argument(
+            "-w",
+            "--words",
+            action="store_true",
+            help="count words in file",
+        )
 
-            self._args = self._parser.parse_args()
-            self._filePath = Path(self._args.file_name).resolve()
-        else:
-            self._filePath = Path(filePath).resolve()
+        self._parser.add_argument(
+            "-L",
+            "--max-line-lenght",
+            action="store_true",
+            help="length of max-length line",
+        )
+
+        self._args = self._parser.parse_args()
+        self._filePath = Path(self._args.file_name).resolve()
 
         if not self._filePath.exists():
             raise FileNotFoundError
@@ -104,7 +103,6 @@ class WC:
             )
 
     def parser_output(self):
-        """Use only with parser (filePath = None)"""
         funcs = [
             self.count_lines,
             self.count_bytes,
@@ -126,7 +124,39 @@ class WC:
                 print(funcs[i]())
 
 
+class WC:
+    """Implementation of linux wc in python,
+    for CLI class consider WCParser
+    """
+
+    def __init__(self, filePath: Union[str, Path]):
+        self._filePath = Path(filePath).resolve()
+        if not self._filePath.exists():
+            raise FileNotFoundError
+
+    def count_lines(self) -> int:
+        with open(self._filePath, encoding="utf-8") as f:
+            return sum(1 for _ in f)
+
+    def count_bytes(self) -> int:
+        stats = os.stat(self._filePath)
+        return stats.st_size
+
+    def count_chars(self) -> int:
+        with open(self._filePath, encoding="utf-8") as f:
+            return sum(len(line) for line in f)  # NOTE: bad!
+
+    def count_words(self) -> int:
+        with open(self._filePath, encoding="utf-8") as f:
+            text = f.read()
+            res = re.findall(r"\w+", text)
+            return len(res)
+
+    def max_line_length(self) -> int:
+        with open(self._filePath, encoding="utf-8") as f:
+            return max(len(line) for line in f)
+
+
 if __name__ == "__main__":
-    wc = WC()
-    # print(wc._args)
+    wc = WCParser()
     wc.parser_output()
